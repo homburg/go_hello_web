d037a0c68a80cb5ee65c714fd3751b3455a17888
Thomas B Homburg <thomas@homburg.dk>
2014-09-03 23:04:51 +0200

    Sqlite db work
    cgo -> slow compile

--- hello.go
+++ hello.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
 	"net/http"
 	"runtime"
 	"strconv"
@@ -22,11 +25,62 @@ func toJson(data interface{}) string {
 }
 
 func main() {
+	log.Println("Starting...")
+
 	runtime.GOMAXPROCS(runtime.NumCPU())
+
 	users := []User{
 		{1, "Brian", 19},
 		{2, "Thomas", 32},
 		{3, "Tonny", 99},
+	}
+
+	// DB conn
+	db, err := sql.Open("sqlite3", "./hello.sqlite3")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY, name VARCHAR(255), age INTEGER)")
+	if nil != err {
+		log.Fatal(err)
+	}
+
+	log.Println("Inserting...")
+	if false {
+		for _, user := range users {
+			_, err = db.Exec("INSERT INTO user (name, age) values (?, ?)", user.Name, user.Age)
+			if nil != err {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	dbUsers, err := db.Query("SELECT * FROM user")
+	if nil != err {
+		log.Fatal(err)
+	}
+	defer dbUsers.Close()
+
+	log.Println("dbUsers")
+	log.Println(dbUsers)
+
+	users = make([]User, 0)
+	for dbUsers.Next() {
+		var id int
+		var name string
+		var age int
+		if dbUsers.Scan(&id, &name, &age); nil != err {
+			log.Fatal(err)
+		}
+
+		dbUser := User{id, name, age}
+
+		log.Println("User")
+		log.Println(dbUser)
+
+		users = append(users, dbUser)
 	}
 
 	r := mux.NewRouter()
